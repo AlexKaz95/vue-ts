0192bfe6512da34cb61d5716e11df88d881a941b
Kazantseva Alexandra <newmoderngoat@gmail.com>
2023-07-24 14:04:22 +0300

    first

---
+++ src/components/CategoryItem.vue
@@ -0,0 +1,49 @@
+<template>
+    <div :class="trClass">
+        <div>
+            {{ item.name }}
+        </div>
+        <div class="justify-self-start">
+            {{ item.limit }}
+        </div>
+        <div class="actions flex gap-2 justify-self-end">
+            <div >
+                edit
+            </div>
+            <div>
+                delete
+            </div>
+        </div>
+    </div>
+    <Teleport to="body" v-if="createOpen">
+        <ModalWindow @click.stop="clickHandler">
+            <CategoryForm :mode="MODE.CREATE" @click.stop="false"/>
+        </ModalWindow>
+    </Teleport>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+
+    type CategoryOptions = {
+        name: string;
+        limit: number;
+    }
+
+    defineProps<{
+        item: CategoryOptions;
+    }>();
+
+    const trClass = computed(() => ([
+        'border-b',
+        'py-2',
+        'px-4',
+        'grid',
+        'grid-cols-3',
+    ]))
+
+</script>
+
+<style scoped>
+
+</style>
